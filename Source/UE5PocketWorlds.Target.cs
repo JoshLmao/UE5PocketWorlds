bba6a0749dfcf5d8cafa8fe46a721e0cb1aa5cc8
Josh Shepherd <josh_lmao@live.co.uk>
2025-08-06 19:34:33 +0100

    Target changes to upgrade

--- Source/UE5PocketWorlds.Target.cs
+++ Source/UE5PocketWorlds.Target.cs
@@ -8,8 +8,8 @@ public class UE5PocketWorldsTarget : TargetRules
 	public UE5PocketWorldsTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
 		ExtraModuleNames.Add("UE5PocketWorlds");
 	}
 }
